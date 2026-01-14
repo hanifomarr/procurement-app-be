a982725fb02818557eee433d3fe59c7627087b51
Hanif Omar <hanif.omarr@gmail.com>
2026-01-14 17:27:07 +0800

    initial commit

---
+++ src/main/java/com/hanifomar/procurement_app/ProcurementAppApplication.java
@@ -0,0 +1,13 @@
+package com.hanifomar.procurement_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementAppApplication.class, args);
+	}
+
+}
