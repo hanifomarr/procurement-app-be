cbfc631da5beaa24feec103aa2cacea44d4641e5
Hanif Omar <hanif.omarr@gmail.com>
2026-01-14 19:19:10 +0800

    feat(security): add JWT-based authentication and authorization

---
+++ src/main/java/com/hanifomar/procurement_app/user/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.hanifomar.procurement_app.user.service;
+
+import com.hanifomar.procurement_app.user.model.User;
+import com.hanifomar.procurement_app.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
