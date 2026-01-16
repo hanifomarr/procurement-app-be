a5d1eda49be699a30a9c087fdea6d7ee65d218ae
Hanif Omar <hanif.omarr@gmail.com>
2026-01-16 19:01:21 +0800

    refactor(supplier): add one-to-many relationship to PurchaseOrder

--- src/main/java/com/hanifomar/procurement_app/supplier/model/Supplier.java
+++ src/main/java/com/hanifomar/procurement_app/supplier/model/Supplier.java
@@ -1,8 +1,12 @@
 package com.hanifomar.procurement_app.supplier.model;
 
+import com.hanifomar.procurement_app.purchaseorder.model.PurchaseOrder;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -27,4 +31,19 @@ public class Supplier {
     private String phone;
 
     private String address;
+
+    @OneToMany(mappedBy = "supplier")
+    private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return Objects.equals(id, supplier.id) && Objects.equals(name, supplier.name) && Objects.equals(contactEmail, supplier.contactEmail) && Objects.equals(phone, supplier.phone) && Objects.equals(address, supplier.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, contactEmail, phone, address);
+    }
 }
